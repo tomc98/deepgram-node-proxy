8c13c3d2723e981bd05ca9b514c6145b309ca620
Tom <tomcsere@gmail.com>
2024-03-15 18:05:16 +1000

    Add Qdrant proxy configuration

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var cors = require("cors");
 require("dotenv").config();
 
 var port = process.env.PORT ?? 8080;
+var qdrantPort = 3001;
 
 var proxy = httpProxy.createProxyServer({
   target: {
@@ -12,6 +13,15 @@ var proxy = httpProxy.createProxyServer({
     host: "api.deepgram.com",
     port: 443,
     pfx: fs.readFileSync("certificate.p12"),
+  },
+  changeOrigin: true,
+});
+
+var qdrantProxy = httpProxy.createProxyServer({
+  target: {
+    protocol: "https:",
+    host: "1cb1d5ed-6fbe-4bc8-8046-c97fe7278208.us-east-1-0.aws.cloud.qdrant.io",
+    port: 6333,
   },
   changeOrigin: true,
 });
@@ -39,6 +49,10 @@ proxy.on("error", function (err, req, res) {
   sendError(res, err);
 });
 
+qdrantProxy.on("error", function (err, req, res) {
+  sendError(res, err);
+});
+
 var corsOptions = {
   origin: process.env.ALLOWED_ORIGIN,
   optionsSuccessStatus: 200,
@@ -48,13 +62,28 @@ proxy.on("proxyRes", (proxyRes, req, res) => {
   cors(corsOptions)(req, res, () => { });
 });
 
+qdrantProxy.on("proxyRes", (proxyRes, req, res) => {
+  cors(corsOptions)(req, res, () => { });
+});
+
 var server = http.createServer(function (req, res) {
   proxy.web(req, res, { target: "https://api.deepgram.com" });
+});
+
+var qdrantServer = http.createServer(function (req, res) {
+  qdrantProxy.web(req, res, { target: "https://1cb1d5ed-6fbe-4bc8-8046-c97fe7278208.us-east-1-0.aws.cloud.qdrant.io:6333" });
 });
 
 server.on("upgrade", function (req, socket, head) {
   proxy.ws(req, socket, head);
 });
 
+qdrantServer.on("upgrade", function (req, socket, head) {
+  qdrantProxy.ws(req, socket, head);
+});
+
 console.log(`listening on port ${port}`);
 server.listen(port);
+
+console.log(`Qdrant proxy listening on port ${qdrantPort}`);
+qdrantServer.listen(qdrantPort);
